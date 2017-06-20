80174ebb015608fa93131d58e213f6677edb7c2a
guyucheng <244077898@qq.com>
2017-06-20 10:44:40 +0800

    Add cut.py, plates_to_test2(3).

--- cut.py
+++ cut.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 img = cv2.imread('plates_to_test3/timg.jpg', -1)
 print(img.shape)
@@ -6,13 +7,23 @@ print(img.shape)
 ret, thresh = cv2.threshold(cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY), 125, 255, cv2.THRESH_BINARY)
 image, contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+
+def sort_func(c):
+    (x, y, w, h) = cv2.boundingRect(c)
+    return (x, y, w, h)
+
+
+contours = sorted(contours, sort_func)
+
 i = 0
 for c in contours:
-    x, y, w, h = cv2.boundingRect(c)
+    (x, y, w, h) = cv2.boundingRect(c)
 
-    if 1500 > w * h > 350:
+    if 350 < w * h < 1500:
         dst = img[y:y + h, x:x + w]  # 输出该区域
-        cv2.imwrite('plates_to_test3/charOut/' + str(i) + 'ch.png', dst)
+        dir = 'plates_to_test3/charOut/'
+        os.system('mkdir -p ' + dir)
+        cv2.imwrite(dir + str(i) + 'ch.png', dst)
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)  # 标注该区域
         i += 1
     print((x, y), (x + w, y + h))
